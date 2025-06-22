04c8a66ea95339910900f7d8fe7ec656a93c6b80
Matias Sayut <matiassayut@gmail.com>
2025-06-22 12:27:55 -0300

    Basic chatbot in homepage

--- src/main/java/com/chatbot/unla/controllers/HomeController.java
+++ src/main/java/com/chatbot/unla/controllers/HomeController.java
@@ -1,15 +1,23 @@
 package com.chatbot.unla.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.chatbot.unla.helpers.ViewRouteHelper;
+import com.chatbot.unla.services.IChatService;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	@Autowired
+	private IChatService chatService;
+
 	
 	//index de la primera vista
 		@GetMapping("/")
@@ -23,6 +31,26 @@ public class HomeController {
 		public ModelAndView indexx() {
 			ModelAndView modelAndView = new ModelAndView(ViewRouteHelper.INDEX);
 			return modelAndView;
+		}
+		
+		@PostMapping("/")
+		public ModelAndView procesarPregunta(@RequestParam("pregunta") String preguntaUsuario) {
+		    ModelAndView modelAndView = new ModelAndView(ViewRouteHelper.INDEX);
+		    String respuestaModelo = chatService.obtenerRespuestaSimilar(preguntaUsuario);
+
+		    String respuestaFinal = "🤐 Pregunta no reconocida. No se responderá nada.";
+		    if (!respuestaModelo.equalsIgnoreCase("NINGUNA")) {
+		        String respuesta = chatService.buscarRespuesta(respuestaModelo);
+		        if (respuesta != null) {
+		            respuestaFinal = "🤖 " + respuesta;
+		        } else {
+		            respuestaFinal = "❌ Pregunta no reconocida: " + respuestaModelo;
+		        }
+		    }
+
+		    modelAndView.addObject("pregunta", preguntaUsuario);
+		    modelAndView.addObject("respuesta", respuestaFinal);
+		    return modelAndView;
 		}
 
 }
